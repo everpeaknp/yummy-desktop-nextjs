e5f894717e52593e244365c1edfb5e689fbf5f68
Ramoniswack <ramontiwari086@gmail.com>
2026-03-06 14:36:34 +0545

    trigger

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'nrrfumuslekbdjvgklqp.supabase.co',
         port: '',
         pathname: '/storage/v1/object/public/**',
